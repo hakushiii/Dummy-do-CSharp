1fee7617e5d6cd4d4c9c43c523523c993458380c
Jerome A <jeromealapad13@gmail.com>
2024-10-21 17:43:10 +0800

    Exercise - for loops

---
+++ Exercise (for)/Program.cs
@@ -0,0 +1,10 @@
+﻿for (int i = 1; i <= 100; i++){
+    string message = "";
+    if (i%3==0){
+        message = message + "Fizz";
+    }
+    if (i%5==0){
+        message = message + "Buzz";
+    }
+    Console.WriteLine($"{i} - {message}");
+}
