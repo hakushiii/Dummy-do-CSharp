8e885ba0788a1f3a61fbec8d4c16daf8bd5a7069
Jerome A <jeromealapad13@gmail.com>
2025-01-11 11:16:14 +0800

    forgor to commit

---
+++ Exercise (string array)/Program.cs
@@ -0,0 +1,14 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string reversedPangram = "";
+
+string[] pangramItems = pangram.Split(" ");
+
+foreach (string item in pangramItems){
+    char[] itemArray = item.ToCharArray();
+    Array.Reverse(itemArray);
+
+    string reversedItem = String.Join("", itemArray);
+    reversedPangram = reversedPangram + reversedItem + " ";
+}
+
+Console.WriteLine(reversedPangram);
---
+++ TestProject/Program.cs
@@ -0,0 +1,21 @@
+﻿string? input;
+
+Console.WriteLine("Insert number between 5 and 10: ");
+do {
+    input = Console.ReadLine();
+    if (input != null){
+        try{
+            int inputNumber = Convert.ToInt32(input);
+            if (inputNumber >=5 && inputNumber <=10){
+                Console.WriteLine($"Input Value ({inputNumber}) has been acknowledged");
+            }
+            else{
+                Console.WriteLine($"You entered ({inputNumber}), a number not in range.");
+            }
+        }
+        catch{
+            Console.WriteLine("Invalid Number");
+        }
+    }
+    
+} while (input == null);
