9b5bfadb6120d219ea0c9f04ac2ff4d3d8170434
Jerome A <jeromealapad13@gmail.com>
2025-01-31 20:21:23 +0800

    returns

---
+++ Exercise (returns)/Program.cs
@@ -0,0 +1,35 @@
+﻿Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay()) 
+{
+    PlayGame();
+}
+
+void PlayGame() 
+{
+    var play = true;
+
+    while (play) 
+    {
+        var target = random.Next(1, 6); 
+        var roll = random.Next(1, 7);
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(roll, target));
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
+
+string WinOrLose(int roll, int target)
+{
+    return (roll > target) ? "You Win!" : "You Lose";
+}
+
+bool ShouldPlay()
+{
+    return (Console.ReadLine().ToLower() == "y") ? true : false;
+}
